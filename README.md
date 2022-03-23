397e0916a1913f742054d04d3b61a1daf507e73e
Usuário <usuario@MacBook-Pro-de-Usuario.local>
2022-03-22 21:47:00 -0300

    commit

---
+++ README.md
@@ -0,0 +1,14 @@
+![tdd_clean_code](https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777184/secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png)
+
+# Using TDD, SOLID and Clean Code in Node API
+ 
+## Technologies
+
+* Node
+* Express
+* Mongo DB
+* Docker
+* Test with Jest
+* Typescript
+
+
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, {json, Request, Response} from 'express'
+import mongoose from 'mongoose'
+
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, PORT } = process.env
+
+const app = express()
+
+// mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+
+// mongoose.connection.on('error', () => console.error('connection error'))
+// mongoose.connection.once('open', () => console.log('database connected'))
+
+app.get('/', (request: Request, response: Response) => {
+    return response.json({
+        message: "Hello World"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log("API is running...")
+})
